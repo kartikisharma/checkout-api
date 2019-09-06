07b9f858436c5903502aabcee088a4cd06128b65
Kartiki Sharma <k35sharm@uwaterloo.ca>
2019-09-05 23:57:29 -0400

    Run through TDD

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+
+  # Validation tests
+  # ensure cols name and available are present before saving
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:available) }
+end
